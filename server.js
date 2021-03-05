d51a92b1622e5b644f66b2b4c9228e639a60cdbc
Silvia Badoz-griffond <silviabadoz-griffond@MacBook-Air-de-Silvia.local>
2021-03-05 23:37:38 +0100

    server

--- server.js
+++ server.js
@@ -13,4 +13,16 @@ res.sendFile(path.join(__dirname+'/dist/connect4-rh-trainees/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080); 
+
+/*function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+const app = express();
+app.use(requireHTTPS);*/
